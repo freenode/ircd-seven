c487c7a327ebbb4875d0dfd6c2f32a4c105c0838
Stephen Bennett <spb@exherbo.org>
2008-07-28 23:11:20 +0000

    Considerably simplify chm_no_oper_kick.c. This merges the setting of +M into the same privilege as +LP, at the benefit of not needing to duplicate chmode.c definitions, nor needing to declare private variables there as public. Revert chmode.c changes.

--- extensions/chm_no_oper_kick.c
+++ extensions/chm_no_oper_kick.c
@@ -10,10 +10,6 @@
 
 static void can_kick(hook_data_channel_approval *);
 
-static void chm_nooperkick(struct Client *source_p, struct Channel *chptr,
-		int alevel, int parc, int *parn, const char **parv,
-		int *errors, int dir, char c, long mode_type);
-
 mapi_hfn_list_av1 nooperkick_hfnlist[] = {
 	{ "can_kick", (hookfn) can_kick },
 	{ NULL, NULL }
@@ -23,7 +19,7 @@ static int
 _modinit(void)
 {
 	chmode_table['M'].mode_type = find_cflag_slot();
-	chmode_table['M'].set_func = chm_nooperkick;
+	chmode_table['M'].set_func = chm_staff;
 
 	construct_noparam_modes();
 
@@ -38,7 +34,7 @@ _moddeinit(void)
 	construct_noparam_modes();
 }
 
-DECLARE_MODULE_AV1(chm_operonly, _modinit, _moddeinit, NULL, NULL, nooperkick_hfnlist, "$Revision$");
+DECLARE_MODULE_AV1(chm_no_oper_kick, _modinit, _moddeinit, NULL, NULL, nooperkick_hfnlist, "$Revision$");
 
 static void
 can_kick(hook_data_channel_approval *data)
@@ -54,66 +50,3 @@ can_kick(hook_data_channel_approval *data)
 		data->approved = 0;
 	}
 }
-
-/* Fairly ugly. Copied from chmode.c. */
-#define SM_ERR_NOPRIVS          0x00000400
-#define MAXMODES_SIMPLE 46 /* a-zA-Z except bqeIov */
-extern struct ChModeChange mode_changes[BUFSIZE];
-extern int mode_count;
-extern int mode_limit;
-extern int mode_limit_simple;
-extern int mask_pos;
-
-
-static void
-chm_nooperkick(struct Client *source_p, struct Channel *chptr,
-		int alevel, int parc, int *parn, const char **parv,
-		int *errors,int dir, char c, long mode_type)
-{
-	if(!IsOper(source_p) && !IsServer(source_p))
-	{
-		if(!(*errors & SM_ERR_NOPRIVS))
-			sendto_one_numeric(source_p, ERR_NOPRIVILEGES, form_str(ERR_NOPRIVILEGES));
-		*errors |= SM_ERR_NOPRIVS;
-		return;
-	}
-	if(MyClient(source_p) && !IsOperImmune(source_p))
-	{
-		if(!(*errors & SM_ERR_NOPRIVS))
-			sendto_one(source_p, form_str(ERR_NOPRIVS), me.name,
-					source_p->name, "immune");
-		*errors |= SM_ERR_NOPRIVS;
-		return;
-	}
-
-	if(MyClient(source_p) && (++mode_limit_simple > MAXMODES_SIMPLE))
-		return;
-
-	/* setting + */
-	if((dir == MODE_ADD) && !(chptr->mode.mode & mode_type))
-	{
-		chptr->mode.mode |= mode_type;
-
-		mode_changes[mode_count].letter = c;
-		mode_changes[mode_count].dir = MODE_ADD;
-		mode_changes[mode_count].caps = 0;
-		mode_changes[mode_count].nocaps = 0;
-		mode_changes[mode_count].id = NULL;
-		mode_changes[mode_count].mems = ALL_MEMBERS;
-		mode_changes[mode_count++].arg = NULL;
-	}
-	else if((dir == MODE_DEL) && (chptr->mode.mode & mode_type))
-	{
-		chptr->mode.mode &= ~mode_type;
-
-		mode_changes[mode_count].letter = c;
-		mode_changes[mode_count].dir = MODE_DEL;
-		mode_changes[mode_count].caps = 0;
-		mode_changes[mode_count].nocaps = 0;
-		mode_changes[mode_count].mems = ALL_MEMBERS;
-		mode_changes[mode_count].id = NULL;
-		mode_changes[mode_count++].arg = NULL;
-	}
-}
-
-
