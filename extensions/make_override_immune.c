6481c47d0a62d74c43e96430e7699727cfba62fe
Stephen Bennett <spb@exherbo.org>
2008-11-26 21:02:50 +0000

    Rename chm_no_oper_kick.so to make_override_immune.so, and remove the +M logic now that that mode exists in core

--- extensions/make_override_immune.c
+++ extensions/make_override_immune.c
@@ -15,26 +15,7 @@ mapi_hfn_list_av1 nooperkick_hfnlist[] = {
 	{ NULL, NULL }
 };
 
-static int
-_modinit(void)
-{
-	chmode_table['M'].mode_type = find_cflag_slot();
-	chmode_table['M'].set_func = chm_staff;
-
-	construct_noparam_modes();
-
-	return 0;
-}
-
-static void
-_moddeinit(void)
-{
-	chmode_table['M'].mode_type = 0;
-
-	construct_noparam_modes();
-}
-
-DECLARE_MODULE_AV1(chm_no_oper_kick, _modinit, _moddeinit, NULL, NULL, nooperkick_hfnlist, "$Revision$");
+DECLARE_MODULE_AV1(chm_no_oper_kick, NULL, NULL, NULL, NULL, nooperkick_hfnlist, "$Revision$");
 
 static void
 can_kick(hook_data_channel_approval *data)
@@ -42,15 +23,6 @@ can_kick(hook_data_channel_approval *data)
 	struct Client *source_p = data->client;
 	struct Client *target_p = data->target;
 	struct Channel *chptr = data->chptr;
-
-	if ((chptr->mode.mode & chmode_flags['M']) && IsOper(target_p) && data->approved) {
-		sendto_one_numeric(source_p, ERR_ISCHANSERVICE,
-				"%s %s :Cannot kick IRC operators from this channel.",
-				target_p->name, chptr->chname);
-		sendto_realops_snomask(SNO_GENERAL, L_NETWIDE, "Overriding KICK from %s on %s in %s (channel is +M)",
-			source_p->name, target_p->name, chptr->chname);
-		data->approved = 0;
-	}
 
 	if (target_p->umodes & UMODE_OVERRIDE && data->approved)
 	{
