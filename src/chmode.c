e590e0d4c2c82ec5a157e32fb15f3f83059e2a96
Stephen Bennett <spb@exherbo.org>
2009-01-15 23:08:40 +0000

    Be less stupid about mode changes by +p opers with channel ops

--- src/chmode.c
+++ src/chmode.c
@@ -1913,7 +1913,7 @@ set_channel_mode(struct Client *client_p, struct Client *source_p,
 
 	mlen = 0;
 
-	for (override = 0; override < (IsOverride(source_p) ? 2 : 1); ++override)
+	for (override = 0; override < (IsOverride(source_p) && alevel != CHFL_CHANOP ? 2 : 1); ++override)
 	{
 		int was_on_chan = 0;
 
