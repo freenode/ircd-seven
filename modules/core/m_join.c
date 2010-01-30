2ba653a3638f7ac7815995ef8583949562185fbb
Stephen Bennett <spb@exherbo.org>
2010-01-30 21:04:33 +0000

    Make default channel mode +ns instead of +nt

--- modules/core/m_join.c
+++ modules/core/m_join.c
@@ -350,11 +350,11 @@ m_join(struct Client *client_p, struct Client *source_p, int parc, const char *p
 				     source_p->name,
 				     source_p->username, source_p->host, chptr->chname);
 
-		/* its a new channel, set +nt and burst. */
+		/* its a new channel, set +ns and burst. */
 		if(flags & CHFL_CHANOP)
 		{
 			chptr->channelts = rb_current_time();
-			chptr->mode.mode |= MODE_TOPICLIMIT;
+			chptr->mode.mode |= MODE_SECRET;
 			chptr->mode.mode |= MODE_NOPRIVMSGS;
 			modes = channel_modes(chptr, &me);
 
