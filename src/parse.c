32714f48fc5f62094aae0cb00d56a2d7d2f37411
mniip <mniip@mniip.com>
2016-12-28 18:10:00 +0300

    Merge m_encap into the ircd core so that it does not cause issues with modrestart

--- src/parse.c
+++ src/parse.c
@@ -41,6 +41,7 @@
 #include "s_conf.h"
 #include "s_serv.h"
 #include "packet.h"
+#include "config.h"
 
 static struct Dictionary *cmd_dict = NULL;
 struct Dictionary *alias_dict = NULL;
@@ -396,6 +397,63 @@ handle_encap(struct Client *client_p, struct Client *source_p,
 	(*handler) (client_p, source_p, parc, parv);
 }
 
+/* ms_encap()
+ *
+ * parv[1] - destination server
+ * parv[2] - subcommand
+ * parv[3] - parameters
+ */
+static int
+ms_encap(struct Client *client_p, struct Client *source_p, int parc, const char *parv[])
+{
+	char buffer[BUFSIZE];
+	char *ptr;
+	int cur_len = 0;
+	int len;
+	int i;
+
+	ptr = buffer;
+
+	for(i = 1; i < parc - 1; i++)
+	{
+		len = strlen(parv[i]) + 1;
+
+		/* ugh, not even at the last parameter, just bail --fl */
+		if((size_t)(cur_len + len) >= sizeof(buffer))
+			return 0;
+
+		rb_snprintf(ptr, sizeof(buffer) - cur_len, "%s ", parv[i]);
+		cur_len += len;
+		ptr += len;
+	}
+
+	len = strlen(parv[i]);
+
+	/* if its a command without parameters, dont prepend a ':' */
+	if(parc == 3)
+		rb_snprintf(ptr, sizeof(buffer) - cur_len, "%s", parv[2]);
+	else
+		rb_snprintf(ptr, sizeof(buffer) - cur_len, ":%s", parv[parc-1]);
+
+	/* add a trailing \0 if it was too long */
+	if((cur_len + len) >= BUFSIZE)
+		buffer[BUFSIZE-1] = '\0';
+
+	sendto_match_servs(source_p, parv[1], CAP_ENCAP, NOCAPS,
+			   "ENCAP %s", buffer);
+
+	/* if it matches us, find a matching handler and call it */
+	if(match(parv[1], me.name))
+		handle_encap(client_p, source_p, parv[2], parc - 2, parv + 2);
+
+	return 0;
+}
+
+struct Message encap_msgtab = {
+	"ENCAP", 0, 0, 0, MFLG_SLOW,
+	{mg_ignore, mg_ignore, {ms_encap, 3}, {ms_encap, 3}, mg_ignore, mg_ignore}
+};
+
 /*
  * clear_hash_parse()
  *
@@ -409,6 +467,7 @@ void
 clear_hash_parse()
 {
 	cmd_dict = irc_dictionary_create(strcasecmp);
+	mod_add_cmd(&encap_msgtab);
 }
 
 /* mod_add_cmd
