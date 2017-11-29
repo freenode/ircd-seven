matrix-construct/construct

Add away-notify client capability.

include/client.h
@@ -444,6 +444,7 @@ struct ListClient
 #define CLICAP_SASL		0x0002
 #define CLICAP_ACCOUNT_NOTIFY	0x0004
 #define CLICAP_EXTENDED_JOIN	0x0008
+#define CLICAP_AWAY_NOTIFY	0x0010
 
 /*
  * flags macros.

freenode/ircd-seven

include/client.h
@@ -467,6 +467,7 @@ struct ListClient
 #define CLICAP_IDENTIFY_MSG	0x0004
 #define CLICAP_ACCOUNT_NOTIFY	0x0008
 #define CLICAP_EXTENDED_JOIN	0x0010
+#define CLICAP_AWAY_NOTIFY	0x0020
 
 /*
  * flags macros.