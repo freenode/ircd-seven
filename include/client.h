matrix-construct/construct

cap: implement ircv3.2 chghost cap

include/client.h
@@ -450,6 +450,7 @@ struct ListClient
 #define CLICAP_TLS			0x0020
 #define CLICAP_USERHOST_IN_NAMES	0x0040
 #define CLICAP_CAP_NOTIFY		0x0080
+#define CLICAP_CHGHOST			0x0100
 
 /*
  * flags macros.

freenode/ircd-seven

include/client.h
@@ -468,6 +468,7 @@ struct ListClient
 #define CLICAP_ACCOUNT_NOTIFY	0x0008
 #define CLICAP_EXTENDED_JOIN	0x0010
 #define CLICAP_AWAY_NOTIFY	0x0020
+#define CLICAP_CHGHOST	0x0040
 
 /*
  * flags macros.