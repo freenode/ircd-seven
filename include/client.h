matrix-construct/construct

m_cap: add 'tls' core capability

include/client.h
@@ -447,6 +447,7 @@ struct ListClient
 #define CLICAP_ACCOUNT_NOTIFY	0x0004
 #define CLICAP_EXTENDED_JOIN	0x0008
 #define CLICAP_AWAY_NOTIFY	0x0010
+#define CLICAP_TLS		0x0020
 
 /*
  * flags macros.

freenode/ircd-seven

include/client.h
@@ -468,6 +468,7 @@ struct ListClient
 #define CLICAP_ACCOUNT_NOTIFY	0x0008
 #define CLICAP_EXTENDED_JOIN	0x0010
 #define CLICAP_AWAY_NOTIFY	0x0020
+#define CLICAP_TLS		0x0040
 
 /*
  * flags macros.